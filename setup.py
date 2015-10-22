3a68ac61d4c41697776c776452e3146488d2e107
Lukasz Grech <lukasz.grech@nokia.com>
2015-10-22 16:39:36 +0200

    nested mapper handles correctly mapping direction

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='MapperPy',
-      version='0.7',
+      version='0.7.1',
       description='Automatic object mapping tool',
       author='Lukasz Grech',
       author_email='mapperpy@gmail.com',
