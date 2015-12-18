21b40eb89aa3d7fcc9b7e15f2394d8c349445d51
Lukasz Grech <lukasz.grech@nokia.com>
2015-12-18 16:24:19 +0100

    updating version to check pip upgrade problem

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='MapperPy',
-      version='0.9.2',
+      version='0.9.3',
       description='Automatic object mapping tool',
       author='Lukasz Grech',
       author_email='mapperpy@gmail.com',
